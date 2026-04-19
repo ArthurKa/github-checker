b7d4d77c48c4bd998f8cbbcabe26f07c33548536
ArthurKa <arthur.katruk@gmail.com>
2026-04-19 16:04:29 +0300

    fix: probably fix sometimes get null as latest tag because of per_page=1

--- apps/api/src/services/fetch/github/githubApiUrls.ts
+++ apps/api/src/services/fetch/github/githubApiUrls.ts
@@ -4,6 +4,6 @@ const apiUrl = 'https://api.github.com';
 
 export const githubApiUrls = {
   repoByName: (repoName: RepoName) => StringURL(`${apiUrl}/repos/${repoName}`),
-  repoReleasesByName: (repoName: RepoName) => StringURL(`${apiUrl}/repos/${repoName}/releases?per_page=1`),
-  repoReleasesById: (repoId: RepoId) => StringURL(`${apiUrl}/repositories/${repoId}/releases?per_page=1`),
+  repoReleasesByName: (repoName: RepoName) => StringURL(`${apiUrl}/repos/${repoName}/releases?per_page=3`),
+  repoReleasesById: (repoId: RepoId) => StringURL(`${apiUrl}/repositories/${repoId}/releases?per_page=3`),
 } as const;
